1233c04417c14c9d7a6833918e04278645a95e89
Minhyung Park <urlinee@gmail.com>
2024-07-17 16:51:00 +0900

    퇴직연금 비교공시 API 추가

--- src/Lifespan/AreaCode.php
+++ src/Lifespan/AreaCode.php
@@ -5,9 +5,10 @@ namespace Minhyung\Fss\Lifespan;
 /**
  * 연금저축 권역 코드
  */
-enum PsAreaCode: int
+enum AreaCode: int
 {
     case BANK = 1;
+    case STOCK = 2;
     case ASSET_MANAGEMENT = 3;
     case LIFE_INSURANCE = 4;
     case NONE_LIFE_INSURANCE = 5;
--- src/Lifespan/Lifespan.php
+++ src/Lifespan/Lifespan.php
@@ -2,6 +2,7 @@
 
 namespace Minhyung\Fss\Lifespan;
 
+use DateTime;
 use GuzzleHttp\Client;
 
 class Lifespan
@@ -24,13 +25,13 @@ class Lifespan
      * 
      * @link   https://www.fss.or.kr/fss/lifeplan/devlopGuide/listInfo1.do?menuNo=201053
      * 
-     * @param  int  $year
-     * @param  int  $quarter
-     * @param  \Minhyung\Fss\Lifespan\PsAreaCode|null $areaCode
+     * @param  int  $year    연도 (YYYY)
+     * @param  int  $quarter 분기 (1~4)
+     * @param  \Minhyung\Fss\Lifespan\AreaCode|null $areaCode  권역
      * @throws \Minhyung\Fss\Lifespan\ApiException
      * @return array
      */
-    public function psCorpList(int $year, int $quarter, ?PsAreaCode $areaCode = null): array
+    public function psCorpList(int $year, int $quarter, ?AreaCode $areaCode = null): array
     {
         $params = [];
         $params['year'] = $year;
@@ -46,13 +47,13 @@ class Lifespan
      * 
      * @link   https://www.fss.or.kr/fss/lifeplan/devlopGuide/listInfo2.do?menuNo=201053
      * 
-     * @param  int  $year
-     * @param  int  $quarter
-     * @param  \Minhyung\Fss\Lifespan\PsAreaCode|null $areaCode
+     * @param  int  $year     연도 (YYYY)
+     * @param  int  $quarter  분기 (1~4)
+     * @param  \Minhyung\Fss\Lifespan\AreaCode|null $areaCode  권역
      * @throws \Minhyung\Fss\Lifespan\ApiException
      * @return array
      */
-    public function psProdList(int $year, int $quarter, ?PsAreaCode $areaCode = null): array
+    public function psProdList(int $year, int $quarter, ?AreaCode $areaCode = null): array
     {
         $params = [];
         $params['year'] = $year;
@@ -68,12 +69,12 @@ class Lifespan
      * 
      * @link  https://www.fss.or.kr/fss/lifeplan/devlopGuide/listInfo3.do?menuNo=201053
      * 
-     * @param  \Minhyung\Fss\Lifespan\PsAreaCode          $areaCode
+     * @param  \Minhyung\Fss\Lifespan\AreaCode          $areaCode
      * @param  \Minhyung\Fss\Lifespan\PsChannelCode|null  $channelCode
      * @throws \Minhyung\Fss\Lifespan\ApiException
      * @return array
      */
-    public function psGuaranteedProdList(PsAreaCode $areaCode, ?PsChannelCode $channelCode = null): array
+    public function psGuaranteedProdList(AreaCode $areaCode, ?PsChannelCode $channelCode = null): array
     {
         $params = [];
         $params['areaCode'] = (int)$areaCode;
@@ -81,6 +82,111 @@ class Lifespan
             $params['channelCode'] = (int)$channelCode;
         }
         return $this->request('psGuaranteedProdList.json', $params);
+    }
+
+    /**
+     * 퇴직연금 비교공시 – 수익률
+     * 
+     * @link  https://www.fss.or.kr/fss/lifeplan/devlopGuide/list2Info1.do?menuNo=201054
+     * 
+     * @param  int      $year    연도 (YYYY)
+     * @param  int      $quarter 분기 (1~4)
+     * @param  int|null $sysType 권역 (1: 원리금 보장, 2: 원리금 비보장, 3: 합계, 4: 자사계열사, 5: 기타사업자)
+     * @throws \Minhyung\Fss\Lifespan\ApiException
+     * @return array
+     */
+    public function rpCorpResultList(int $year, int $quarter, ?int $sysType = null): array
+    {
+        $params = [];
+        $params['year'] = $year;
+        $params['quarter'] = $quarter;
+        if ($sysType) {
+            $params['sysType'] = $sysType;
+        }
+        return $this->request('rpCorpResultList.json', $params);
+    }
+
+    /**
+     * 퇴직연금 비교공시 – 총비용 부담률
+     * 
+     * @link  https://www.fss.or.kr/fss/lifeplan/devlopGuide/list2Info2.do?menuNo=201054
+     * 
+     * @param  int  $year  연도 (YYYY)
+     * @throws \Minhyung\Fss\Lifespan\ApiException
+     * @return array
+     */
+    public function rpCorpBurdenRatioList(int $year): array
+    {
+        $params = [];
+        $params['year'] = $year;
+        return $this->request('rpCorpBurdenRatioList.json', $params);
+    }
+
+    /**
+     * 퇴직연금 비교공시 - 맞춤형 수수료 비교
+     * 
+     * @link  https://www.fss.or.kr/fss/lifeplan/devlopGuide/list2Info3.do?menuNo=201054
+     * 
+     * @param  int      $reserve  적립금액 (백만 단위 입력)
+     * @param  int|null $sysType  제도 유형(1: DB, 2: DC, 3: 기업형 IRP, 4: 개인형 IRP(가입자 부담분), 5: 표준형 DC, 6: 연금전환특약, 7: 개인형 IRP(사용자 부담분))
+     * @param  int|null $term     장기 계약
+     * @throws \Minhyung\Fss\Lifespan\ApiException
+     * @return array
+     */
+    public function rpCorpCustomFeeList(int $reserve, ?int $sysType = null, ?int $term = null): array
+    {
+        $params = [];
+        $params['reserve'] = $reserve;
+        if ($sysType) {
+            $params['sysType'] = $sysType;
+        }
+        if ($term) {
+            $params['term'] = $term;
+        }
+        return $this->request('rpCorpCustomFeeList.json', $params);
+    }
+
+    /**
+     * 퇴직연금 비교공시 - 원리금보장상품 제공현황
+     * 
+     * @link  https://www.fss.or.kr/fss/lifeplan/devlopGuide/list2Info4.do?menuNo=201054
+     * 
+     * @param  \Minhyung\Fss\Lifespan\AreaCode|null $areaCode  권역
+     * @throws \Minhyung\Fss\Lifespan\ApiException
+     * @return array
+     */
+    public function rpGuaranteedProdSupplyList(?AreaCode $areaCode = null): array
+    {
+        $params = [];
+        if ($areaCode) {
+            $params['areaCode'] = (int)$areaCode;
+        }
+        return $this->request('rpGuaranteedProdSupplyList.json', $params);
+    }
+
+    /**
+     * 퇴직연금 비교공시 - 원리금보장 퇴직연금상품
+     * 
+     * @link  https://www.fss.or.kr/fss/lifeplan/devlopGuide/list2Info5.do?menuNo=201054
+     * 
+     * @param  \Minhyung\Fss\Lifespan\AreaCode  $areaCode  권역
+     * @param  int       $sysType     제도 유형 (1: DB, 2: DC, 3: IRP)
+     * @param  int       $reportYear  기준 시점 연도 (YYYY)
+     * @param  int       $reportMonth 기준 시점 달 (1~12)
+     * @param  int|null  $productType 상품 유형 (1: 은행 예적금, 2: 저축은행 예적금, 3: 우체국 예적금, 4: 금리연동형 보험, 5: 이율보증형 보험, 6: 정부보증채, 7: 원리금파생상품결합사채, 8: 환매조건부 매수계약)
+     * @throws \Minhyung\Fss\Lifespan\ApiException
+     * @return array
+     */
+    public function rpGuaranteedProdList(AreaCode $areaCode, int $sysType, int $reportYear, int $reportMonth, ?int $productType = null): array
+    {
+        $params = [];
+        $params['areaCode'] = (int)$areaCode;
+        $params['sysType'] = $sysType;
+        $params['reportDate'] = (new DateTime())->setDate($reportYear, $reportMonth, 1)->format('Y/m');
+        if ($productType) {
+            $params['productType'] = $productType;
+        }
+        return $this->request('rpGuaranteedProdList.json', $params);
     }
 
     /**
