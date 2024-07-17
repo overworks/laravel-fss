d3edc38d9028d9151daf02b3d5c1a901d339205f
Minhyung Park <urlinee@gmail.com>
2024-07-17 15:13:50 +0900

    기본 파일 추가

---
+++ config/fss.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'lifespan' => [
+        'api_key' => env('FSS_LIFESPAN_API_KEY', ''),
+    ],
+];
---
+++ src/ServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Minhyung\Fss;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/fss.php', 'fss');
+    }
+
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/fss.php' => config_path('fss.php'),
+            ], 'fss');
+        }
+    }
+}
