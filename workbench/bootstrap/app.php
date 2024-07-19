e428724d4e387e037b90bb5f75b5bf29cca6d0e0
Minhyung Park <urlinee@gmail.com>
2024-07-19 15:48:47 +0900

    불필요한 설정 삭제

--- workbench/bootstrap/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Foundation\Application;
-
-use function Orchestra\Testbench\default_skeleton_path;
-
-return Application::configure(basePath: $APP_BASE_PATH ?? default_skeleton_path())
-    ->create();
